f30523c58d87759dfe25880fcd762e3a1cb3fd1e
Daniel Gray <yodan.and.the.sixth.character@gmail.com>
2024-07-11 16:40:01 -0400

    v0.0.3: Protobuf and Vuejs frontend hello world

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-v0.0.2: Spring hello world backend
+v0.0.3: Protobuf and Vuejs frontend hello world
 
 ### Backend
 
@@ -11,3 +11,18 @@ Run packaged:\
 `java -jar target/backend-0.0.1-SNAPSHOT.jar`
 
 ### Frontend
+
+Install node modules:\
+`npm i`
+
+Format code:\
+`npm run format`
+
+Build static site:\
+`npm run build`
+
+
+### Model
+
+Compile protobuf types:\
+`protoc --java_out=../backend/src/main/java/fyi/lnz/psych_constructs/model/proto ./addressbook.proto`
--- backend/src/main/java/fyi/lnz/psych_constructs/controller/TestController.java
+++ backend/src/main/java/fyi/lnz/psych_constructs/controller/TestController.java
@@ -1,20 +1,21 @@
 package fyi.lnz.psych_constructs.controller;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import fyi.lnz.psych_constructs.model.TestModel;
+import proto.Person;
 
 @RestController
 public class TestController {
 	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
+	private final AtomicInteger counter = new AtomicInteger();
 
 	@GetMapping("/test")
-	public TestModel greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new TestModel(counter.incrementAndGet(), String.format(template, name));
+	public byte[] greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return Person.newBuilder().setName(String.format(template, name)).setId(counter.incrementAndGet()).build().toByteArray();
 	}
 }
